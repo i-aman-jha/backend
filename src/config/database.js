2697e58aa31757a7a3f69f6ce7359e5a0873292d
Aman Jha <150509057+i-aman-jha@users.noreply.github.com>
2024-11-21 01:03:46 +0530

    first commit

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 module.exports = {
     connect : async()=>{
-        await mongoose.connect('mongodb://localhost/anonymonous_backend');
+        await mongoose.connect(process.env.MONGO_URI);
     }
 }
